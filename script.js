e196844fc6f07e36cbf3761c2de88446c9b5b027
beko-06 <140330991+beko-06@users.noreply.github.com>
2025-06-23 21:01:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,26 @@
 const envelope = document.getElementById("envelope");
-const message = document.getElementById("message");
+const flap = envelope.querySelector(".flap");
 const ring = document.getElementById("ring");
+const message = document.getElementById("message");
 const showBtn = document.getElementById("showBtn");
 
-// Butona tıklanınca animasyon başlar
 showBtn.addEventListener("click", () => {
-  // Zarf açılır
-  const flap = envelope.querySelector(".flap");
   flap.style.transform = "rotateX(180deg)";
-
-  // Yüzük görünür
   ring.classList.remove("hidden");
   ring.classList.add("animate");
-
-  // Mesaj görünür
   message.classList.remove("hidden");
   setTimeout(() => {
     message.style.opacity = 1;
   }, 500);
-
-  // Konfeti ve havai fişekler
   startConfettiLoop();
 });
 
-// Konfeti patlatıcı
 function startConfettiLoop() {
   setInterval(() => {
     confetti({
       particleCount: 100,
-      spread: 160,
-      origin: { y: Math.random() * 0.6 },
+      spread: 150,
+      origin: { y: Math.random() * 0.8 },
     });
-  }, 1500);
+  }, 2000);
 }
